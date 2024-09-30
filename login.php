c6315d9ec4a889bdad6eea4c39dc407a281cd082
jaroslank <jaroslank@hotmail.com>
2024-09-30 20:43:44 -0300

    Adicionado functions e login.php

--- login.php
+++ login.php
@@ -1,38 +1,11 @@
-<?php require_once "functions.php";?>
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário de acesso ao site</title>
-    <link rel="stylesheet" href="docs/login.css">
+    <title>Document</title>
 </head>
 <body>
-    <div class="main-login">
-        <div class="left-login">
-            <h1>PigHemp®<br>Painel Administrativo</h1>
-            <img src="img/computer-troubleshooting-animate.svg" class="left-login-img" alt="Computador animado">
-        </div>
-        <div class="right-login">
-            <form action="" method="post" class="card-login">
-                <h1>LOGIN</h1>
-                <div class="textfield">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" placeholder="Informe seu e-mail" required>
-                </div>
-
-                <div class="textfield">
-                    <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Digite sua senha" required>
-                </div>
-                <input class="btn-login" type="submit" name="acessar" value="Acessar">
-            </form>
-        </div>
-    </div>
-    <?php 
-        if (isset($_POST['acessar'])) {
-            login($connect);
-        }
-    ?>
+    <?php ?>
 </body>
 </html>
